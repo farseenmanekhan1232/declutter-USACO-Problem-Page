a04b42b8cbf023a9bc150d988ddef78b8c8c7242
mohdfarseen <farseenmanekhan1232@gmail.com>
2022-09-23 20:13:32 +0530

    improved DOM manipulations

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,10 +1,9 @@
 function reddenPage() {
   if (window.sessionStorage.getItem("declutterUSACO") == "yes") {
     window.sessionStorage.setItem("declutterUSACO", "no");
-
     document.querySelectorAll("div")[0].style["width"] = "6.25in";
   } else {
-    window.sessionStorage.setItem("declutterOnlineJudge", "yes");
+    window.sessionStorage.setItem("declutterUSACO", "yes");
     document.querySelectorAll("div")[0].style["width"] = "80%";
   }
 }
@@ -12,12 +11,11 @@ function reddenPage() {
 chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
   let url = tabs[0].url;
   console.log(url);
-  window.sessionStorage("declutterOnlineJudge", "yes");
-  window;
+  window.sessionStorage("declutterUSACO", "yes");
 });
 
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.includes("https://onlinejudge.org/")) {
+  if (tab.url.includes("https://train.usaco.org/")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: reddenPage,
